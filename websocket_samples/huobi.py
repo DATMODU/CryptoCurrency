eedf440037027f4eb3a33a7b7c40c0e9a715bc60
3x3x3 <ivmivm001@gmail.com>
2018-04-05 01:07:43 +0900

    50번만 받게 수정

--- websocket_samples/huobi.py
+++ websocket_samples/huobi.py
@@ -5,43 +5,70 @@ import json
 import sys
 
 if "__main__" == __name__:
-	ws = None
 
-	# Socket Connection
+	# WebSocket Connection
+	ws = websocket.WebSocket()
 	try:
-		ws = websocket.create_connection("wss://api.huobipro.com/ws")
+		ws.connect("wss://api.huobipro.com/ws")
 	except:
 		print("Failed to Create Connection")
 		sys.exit()
 
-	# Candlestick Data
-	req_dict = {
-		"sub": "market.ethusdt.kline.1min",
-		"id": "client_area : Candlestick Data"
-	}
-
-	req_json = json.dumps(req_dict)
-	ws.send(req_json)
-	print(req_json)
-
 	# Subscribe Market Depth
 	req_dict = {
 		"sub": "market.ethbtc.depth.step0",
-		"id": "client_area : Subscribe Market Depth"
+		"id": "Subscribe Market Depth (ethbtc)"
 	}
 
 	req_json = json.dumps(req_dict)
 	ws.send(req_json)
 	print(req_json)
 
-	# Receive
-	while True:
+	# Subscribe Trade Detail
+	req_dict = {
+		"sub": "market.ethbtc.trade.detail",
+		"id": "Subscribe Trade Detail (ethbtc)"
+	}
+
+	req_json = json.dumps(req_dict)
+	ws.send(req_json)
+	print(req_json)
+
+	# Receive 50 times
+	unsub_cnt = 0
+	while 50 > unsub_cnt:
 		rcv_json = gzip.decompress(ws.recv()).decode('utf-8')
 		rcv_dict = json.loads(rcv_json)
+
+		print(rcv_dict)
 
 		if "ping" in rcv_dict:
 			req_dict = {"pong": rcv_dict["ping"]}
 			req_json = json.dumps(req_dict)
 			ws.send(req_json)
 
-		print(rcv_dict)
+		unsub_cnt = unsub_cnt + 1
+
+	# Unsubscribe Market Depth
+	req_dict = {
+		"unsub": "market.ethbtc.depth.step0",
+		"id": "Unsubscribe Market Depth (ethbtc)"
+	}
+
+	req_json = json.dumps(req_dict)
+	ws.send(req_json)
+	print(req_json)
+
+	# Unsubscribe Trade Detail
+	req_dict = {
+		"unsub": "market.ethbtc.trade.detail",
+		"id": "Unsubscribe Trade Detail (ethbtc)"
+	}
+
+	req_json = json.dumps(req_dict)
+	ws.send(req_json)
+	print(req_json)
+
+	# WebSocket Close
+	ws.close()
+	print("WebSocket Closed")
--- websocket_samples/okcoin.py
+++ websocket_samples/okcoin.py
@@ -4,15 +4,16 @@ import json
 import sys
 
 if "__main__" == __name__:
-	ws = None
 
+	# Socket Connection
+	ws = websocket.WebSocket()
 	try:
-		ws = websocket.create_connection("wss://real.okcoin.com:10440/websocket")
+		ws.connect("wss://real.okcoin.com:10440/websocket")
 	except:
 		print("Failed to Create Connection")
 		sys.exit()
 
-	# Ticker
+	# Add Ticker Channel
 	req_dict = {
 		"event": "addChannel",
 		"channel": "ok_sub_spot_btc_usd_ticker"
@@ -22,7 +23,7 @@ if "__main__" == __name__:
 	ws.send(req_json)
 	print(req_json)
 
-	# Depth
+	# Add Depth Channel
 	req_dict = {
 		"event": "addChannel",
 		"channel": "ok_sub_spot_btc_usd_depth"
@@ -32,8 +33,35 @@ if "__main__" == __name__:
 	ws.send(req_json)
 	print(req_json)
 
-	# Receive
-	while True:
+	# Receive 50 times
+	unsub_cnt = 0
+	while 50 > unsub_cnt:
 		rcv_json = ws.recv()
 		rcv_dict = json.loads(rcv_json)
 		print(rcv_dict)
+
+		unsub_cnt = unsub_cnt + 1
+
+	# Remove Ticker Channel
+	req_dict = {
+		"event": "removeChannel",
+		"channel": "ok_sub_spot_btc_usd_ticker"
+	}
+
+	req_json = json.dumps(req_dict)
+	ws.send(req_json)
+	print(req_json)
+
+	# Remove Depth Channel
+	req_dict = {
+		"event": "removeChannel",
+		"channel": "ok_sub_spot_btc_usd_depth"
+	}
+
+	req_json = json.dumps(req_dict)
+	ws.send(req_json)
+	print(req_json)
+
+	# WebSocket Close
+	ws.close()
+	print("WebSocket Closed")
