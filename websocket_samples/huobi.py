30780e15e4d881965aaf93cada8ddae273dbdd07
NOTEBOOK-IVM\ivmiv <ivmivm@naver.com>
2018-04-05 00:27:22 +0900

    huobi sample 추가

---
+++ websocket_samples/huobi.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+import websocket
+import gzip
+import json
+import sys
+
+if "__main__" == __name__:
+	ws = None
+
+	try:
+		ws = websocket.create_connection("wss://api.huobipro.com/ws")
+	except:
+		print("Failed to Create Connection")
+		sys.exit()
+
+	# Candlestick Data
+	req_dict = {
+		"sub": "market.ethusdt.kline.1min",
+		"id": "client_area : Candlestick Data"
+	}
+
+	req_json = json.dumps(req_dict)
+	ws.send(req_json)
+	print(req_json)
+
+	# Subscribe Market Depth
+	req_dict = {
+		"sub": "market.ethbtc.depth.step0",
+		"id": "client_area : Subscribe Market Depth"
+	}
+
+	req_json = json.dumps(req_dict)
+	ws.send(req_json)
+	print(req_json)
+
+	# Receive
+	while True:
+		rcv_json = gzip.decompress(ws.recv()).decode('utf-8')
+		rcv_dict = json.loads(rcv_json)
+
+		if "ping" in rcv_dict:
+			req_dict = {"pong": rcv_dict["ping"]}
+			req_json = json.dumps(req_dict)
+			ws.send(req_json)
+
+		print(rcv_dict)
